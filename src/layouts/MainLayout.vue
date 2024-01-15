19c56b1b0e40d737ae7797a35b9c037cc7e3558e
Noxxider <ravino.juwono@gmail.com>
2024-01-15 12:02:02 -0800

    Add graph dependency

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,37 +1,78 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout class="layout" view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar style="align-items: center">
+        <div class="small-screen-only q-mx-auto full-width">
+          <div class="flex justify-between">
+            <q-btn to="/" flat>
+              <img
+                src="~assets/header.svg"
+                class="q-my-auto"
+                style="height: 24px; padding: 0px"
+              />
+            </q-btn>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+            <q-btn
+              flat
+              no-caps
+              dense
+              icon="menu"
+              style="color: #262626"
+              aria-label="Menu"
+              @click="toggleLeftDrawer"
+              class="small-screen-only q-my-auto"
+            />
+          </div>
+        </div>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div
+          class="large-screen-only"
+          style="display: flex; align-items: center"
+        >
+          <q-btn to="/" class="large-screen-only q-ml-md" flat>
+            <img
+              src="~src\assets\header.svg"
+              class="q-my-auto"
+              style="height: 24px; padding: 0px"
+            />
+          </q-btn>
+          <q-btn
+            class="large-screen-only q-px-xl q-mx-xl"
+            no-caps
+            to="/"
+            text-color="dark"
+            label="Home"
+            style="padding: 8px 12px"
+            unelevated
+          />
+          <q-btn
+            class="large-screen-only q-px-xl"
+            no-caps
+            to="/contact"
+            text-color="dark"
+            label="Contact"
+            style="padding: 8px 12px"
+            unelevated
+          />
+          <q-btn
+            class="large-screen-only q-ml-xl"
+            no-caps
+            to="/resume"
+            color="accent"
+            label="Resume"
+            style="padding: 8px 16px"
+            unelevated
+          />
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" :width="200" bordered behavior="mobile">
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
+        <div class="text-h6 q-pa-md" header>Pages</div>
+        <q-separator />
         <EssentialLink
+          class="q-mt-sm"
           v-for="link in essentialLinks"
           :key="link.title"
           v-bind="link"
@@ -46,71 +87,71 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-    EssentialLink
+    EssentialLink,
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,14 +4,42 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
+    <div>
+      <ApexCharts
+        width="500"
+        type="bar"
+        :options="options"
+        :series="series"
+      ></ApexCharts>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import ApexCharts from "vue3-apexcharts";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  components: { ApexCharts },
+  data() {
+    return {
+      options: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+  },
+});
 </script>
